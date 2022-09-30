8faba94c28d22e559904adf32f49ccfd267f3797
Vanessa Soares <vanessafsoares@Vanessas-M1-2.local>
2022-09-30 17:26:48 -0300

    add jest and detox configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,20 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  env: {
+    node: true,
+    commonjs: true,
+    jest: true,
+    mocha: true,
+    browser: true,
+  },
+  plugins: [
+    'import',
+    'react',
+    'react-native',
+    'detox',
+    'testing-library',
+    'eslint-plugin-local-rules',
+    'prettier',
+  ],
 };
